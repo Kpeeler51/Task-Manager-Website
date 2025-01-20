33e8cbbdc593a2ac96a67a7286756a34d9995f1c
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-19 16:56:28 -0800

    homepage header

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <header>
+        <h1>Task Management Made Easy</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Blanditiis assumenda, dolorum deserunt sequi
+            voluptatem cupiditate quo exercitationem sunt labore.
+        </p>
+    </header>
   )
 }
