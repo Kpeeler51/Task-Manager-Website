3d2c0f768709bccdaf0799516bead5a71426ff59
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-19 16:35:15 -0800

    Routes and navbar added to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
-
+import {BrowserRouter, Route, Routes} from 'react-router'
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Tasks from './components/Tasks/Tasks';
+import Contact from './components/Contact/Contact';
 function App() {
 
   return (
     <>
+   <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Tasks />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
