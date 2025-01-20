c34b53735b54e9d8cd207dabba4fffe87ddfe260
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-20 14:33:08 -0800

    click handling logic added to tasks page.

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -3,15 +3,23 @@ import './Tasks.css'
 import TaskContainer from "../TaskContainer/TaskContainer"
 import TaskWindow from "../TaskWindow/TaskWindow"
 
+interface Task {
+  task: string;
+  desc: string;
+}
+
 export default function Tasks() {
-    const [tasks, setTasks] = useState ([
-      {task:'chores', desc: 'take out trash'},
-      {task: 'clean', desc: 'clean room'},
-      {task: 'activity', desc: 'physical activity'},  ]);
-    
-    
+  const [tasks, setTasks] = useState<Task[]>([
+    {task:'chores', desc: 'take out trash'},
+    {task: 'clean', desc: 'clean room'},
+    {task: 'activity', desc: 'physical activity'},
+  ]);
 
+  const [selectedTask, setSelectedTask] = useState<Task | null>(null);
 
+  const handleTaskClick = (task: Task) => {
+    setSelectedTask(task);
+  };
 
   return (
     <>
