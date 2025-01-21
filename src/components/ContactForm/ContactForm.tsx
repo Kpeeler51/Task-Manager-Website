32837cc31b90b0172a53b9e9e6ee447570d2d360
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-20 18:39:13 -0800

    classnames for contactform

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -3,14 +3,14 @@ export default function ContactForm() {
   return (
     <div>
         <form>
-            <div>
+            <div className="nameform">
             <input type="text" placeholder="First Name"/>
             <input type="text" placeholder="Last Name"/>
             </div>
-            <div>
+            <div className="mailform">
             <input type="email" placeholder="Email"/>
             </div>
-            <div>
+            <div className="messageform">
             <textarea placeholder="Message"></textarea>
             </div>
             <button type="submit">Submit</button>
