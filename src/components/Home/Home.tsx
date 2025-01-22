5970a0a7ff110ab03e60e236d03cfeed2b639708
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-21 17:18:06 -0800

    home page customization

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,29 +6,29 @@ export default function Home() {
   return (
     <>
     <header className='home-header'>
-        <h1>Task Management Made Easy</h1>
+        <h1 className='header-title'>Task Management Made Easy</h1>
         <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Blanditiis assumenda, dolorum deserunt sequi
             voluptatem cupiditate quo exercitationem sunt labore.
         </p>
 
-        <button><Link to="/tasks">Find out more!</Link></button>
+        <button className='header-link'><Link to="/tasks">Find out more!</Link></button>
     </header>
 
     <main className='home-info'>
-        <section className='home-item'>
-            <h2> Goal </h2>
+        <section className='home-container'>
+            <h2 className='main-header'> Goal </h2>
             <Dropdown>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ut ipsum quos consequuntur obcaecati soluta aspernatur aliquid est earum ad. Facilis omnis maxime quaerat quis nam reprehenderit architecto unde animi!
+                <p className='home-item'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ut ipsum quos consequuntur obcaecati soluta aspernatur aliquid est earum ad. Facilis omnis maxime quaerat quis nam reprehenderit architecto unde animi!
                 A atque sapiente reiciendis nisi aperiam ducimus, ipsam veniam vitae tempora vero delectus fugiat magni quod aliquid facere quia maxime voluptatem temporibus nihil possimus sunt? Soluta deleniti quasi facilis tenetur.
                 Voluptas magni non, eligendi voluptatibus voluptate aspernatur vero? Exercitationem, odit! Autem eligendi ipsa saepe voluptas ullam. Adipisci inventore autem reprehenderit deserunt repudiandae maxime labore neque! Libero beatae suscipit placeat cupiditate.</p>
             </Dropdown>
         </section>
-        <section className='home-item'>
-            <h2> Supporting Statement</h2>
+        <section className='home-container'>
+            <h2 className='main-header'> Supporting Statement</h2>
             <Dropdown>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ut ipsum quos consequuntur obcaecati soluta aspernatur aliquid est earum ad. Facilis omnis maxime quaerat quis nam reprehenderit architecto unde animi!
+                <p className='home-item'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ut ipsum quos consequuntur obcaecati soluta aspernatur aliquid est earum ad. Facilis omnis maxime quaerat quis nam reprehenderit architecto unde animi!
                 A atque sapiente reiciendis nisi aperiam ducimus, ipsam veniam vitae tempora vero delectus fugiat magni quod aliquid facere quia maxime voluptatem temporibus nihil possimus sunt? Soluta deleniti quasi facilis tenetur.
                 Voluptas magni non, eligendi voluptatibus voluptate aspernatur vero? Exercitationem, odit! Autem eligendi ipsa saepe voluptas ullam. Adipisci inventore autem reprehenderit deserunt repudiandae maxime labore neque! Libero beatae suscipit placeat cupiditate.</p>
             </Dropdown>
