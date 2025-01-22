57dbc75303f5b865436693969d5d8fa445133d13
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-21 16:23:24 -0800

    dropdown icons and icon customization

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import './Dropdown.css'
+import upArrow from '../../assets/icons/iconup50.png'
+import downArrow from '../../assets/icons/icondown50.png'
 
 export default function Dropdown({children}: {children: React.ReactNode}) {
     const [isOpen, setOpen] = useState(false);
@@ -11,7 +13,11 @@ export default function Dropdown({children}: {children: React.ReactNode}) {
     return (
         <div className="dropdown">
             <button onClick={toggleDropdown} className="dropdown-toggle">
-                {isOpen ? '▲' : '▼'}
+                <img 
+                    src={isOpen ? upArrow : downArrow} 
+                    alt={isOpen ? "Close dropdown" : "Open dropdown"}
+                    className="dropdown-icon"
+                />
             </button>
             {isOpen && <div className="dropdown-content">{children}</div>}
         </div>
