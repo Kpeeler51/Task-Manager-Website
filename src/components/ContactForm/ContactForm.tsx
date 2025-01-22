8f6642f0fe6fc417bb2c469e28e3cc3bea7f1840
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-21 20:08:34 -0800

    contact form customization

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,3 +1,4 @@
+import './ContactForm.css'
 import { useState, ChangeEvent, FormEvent } from 'react';
 
 interface FormData {
@@ -40,8 +41,10 @@ export default function ContactForm({ onSubmit }: ContactFormProps) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="nameform">
+      <div className="name-form">
+        <label> First Name:
         <input
+          className='name-field'
           type="text"
           name="firstName"
           value={formData.firstName}
@@ -49,7 +52,10 @@ export default function ContactForm({ onSubmit }: ContactFormProps) {
           placeholder="First Name"
           aria-label="First Name"
         />
+        </label>
+        <label> Last Name:
         <input
+          className='name-field'
           type="text"
           name="lastName"
           value={formData.lastName}
@@ -57,27 +63,34 @@ export default function ContactForm({ onSubmit }: ContactFormProps) {
           placeholder="Last Name"
           aria-label="Last Name"
         />
+        </label>
       </div>
-      <div className="mailform">
+      <div className="mail-form">
+        <label> Email:
         <input
+          className='mail-field'
           type="email"
           name="email"
           value={formData.email}
           onChange={handleChange}
           placeholder="Email"
           aria-label="Email"
+          size={60}
         />
+        </label>
       </div>
-      <div className="messageform">
+      <div className="message-form">
+        <label htmlFor='message' className='message-label'> Message: </label>
         <textarea
           name="message"
           value={formData.message}
           onChange={handleChange}
           placeholder="Message"
           aria-label="Message"
+          rows={10} cols={60}
         ></textarea>
       </div>
-      <button type="submit">Submit</button>
+      <button className='submit-button' type="submit">Submit</button>
     </form>
   );
 }
