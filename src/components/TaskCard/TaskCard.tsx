bc7f95bf18529a9d9043b6b31e643a3ca469d9cd
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-22 17:45:32 -0800

    reformatted taskcard for better checkbox and delete placement

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -18,25 +18,25 @@ export default function TaskCard({
    task, onTaskClick, onComplete, onDelete }: TaskCardProps) {
   return (
     <div className="taskcard" onClick={onTaskClick}>
-      <h3 className={task.completed ? 'completed' : ''}>
-        {task.task}
-      </h3>
-      <input
-        type="checkbox"
-        className='checkbox-input'
-        checked={task.completed}
-        onChange={(e) => {
-          e.stopPropagation();
-          onComplete();
-        }}
-      />
+      <div className="taskcard-content">
+        <h3 className={task.completed ? 'completed' : ''}>
+          {task.task}
+        </h3>
+        <input
+          type="checkbox"
+          className='checkbox-input'
+          checked={task.completed}
+          onChange={(e) => {
+            e.stopPropagation();
+            onComplete();
+          }}/>
+      </div>
       <button 
         className="delete-button" 
         onClick={(e) => {
           e.stopPropagation();
           onDelete();
-        }}
-      >
+        }}>
         <img src={Delete} alt="Delete" />
       </button>
     </div>
