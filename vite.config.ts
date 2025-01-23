4a5cd13459958ecd00256ab676d2aa9b5ecc5771
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-22 21:52:38 -0800

    fixed github pages not working

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://kpeeler51.github.io/Task-Manager-Website/",
 })
