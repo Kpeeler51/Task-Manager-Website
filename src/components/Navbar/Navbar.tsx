510f41918aa338241054192ab5271f0cda6e4da4
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-22 18:13:51 -0800

    Comments added to explain navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 import {Link} from 'react-router';
 import './Navbar.css';
-
+// Navbar component that renders navigation links.
 export default function Navbar() {
   return (
     <nav>
+      {/* Router links to different pages. Logo links back to home page. */}
       <Link className="logo" to="/">TaskManager</Link>
       <div className="nav-links">
         <Link className="nav-item" to="/">Home</Link>
