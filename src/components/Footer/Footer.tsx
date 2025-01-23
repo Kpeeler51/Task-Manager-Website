ca65e770fd0057f0528c76256c8efa985ea0d275
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-22 20:56:46 -0800

    added comments to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,10 @@ import './Footer.css'
 export default function Footer() {
   return (
     <footer>
+        {/* Copyright Text */}
         <p className='copyright'>Copyright &copy; 2025 TaskManager. All rights reserved.</p>
-        <p className='icons'>Icons by <a href="https://icons8.com/">Icons8</a></p>
+        {/* Copyright text required for usage of Icons8 icons. */}
+        <p className='icons'>Icons by <a href="https://icons8.com/">Icons8</a></p> 
     </footer>
   )
 }
