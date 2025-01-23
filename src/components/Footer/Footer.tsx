1f4f38d186d1b0ff84b97258565a7151e6113eee
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-22 20:34:22 -0800

    footer added

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <footer>
+        <p className='copyright'>Copyright &copy; 2025 TaskManager. All rights reserved.</p>
+        <p className='icons'>Icons by <a href="https://icons8.com/">Icons8</a></p>
+    </footer>
+  )
+}
