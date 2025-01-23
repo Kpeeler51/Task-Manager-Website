60a9c9fe3a65c7492897303b4944bcd923757842
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-22 18:42:18 -0800

    comments added explaining TaskDetails

--- src/components/TaskDetails/TaskDetails.tsx
+++ src/components/TaskDetails/TaskDetails.tsx
@@ -1,18 +1,22 @@
 import './TaskDetails.css';
 
+// Define the type and structure for the tasks.
 interface Task {
   task: string;
   desc: string;
 }
-
+ // Define the props for the TaskDetails component.
 interface TaskDetailsProps {
   task: Task;
 }
 
+// TaskDetails component that renders the task details into the TaskWindow.
 export default function TaskDetails({ task }: TaskDetailsProps) {
   return (
     <div className="task-details">
+      {/* Task Title. */}
       <h2 className='task-title'>{task.task}</h2>
+      {/* Task Title */}
       <p className='task-desc'>{task.desc}</p>
     </div>
   )
