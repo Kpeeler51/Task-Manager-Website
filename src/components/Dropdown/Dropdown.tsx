5b4d153286ddfccb187fc4986c413431ceeb94cc
Kyle Peeler <kylepeeler@yahoo.com>
2025-01-22 18:12:08 -0800

    comments added to explain homepage and its dropdown code.

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -3,22 +3,28 @@ import './Dropdown.css';
 import upArrow from '../../assets/icons/iconup50.png';
 import downArrow from '../../assets/icons/icondown50.png';
 
+// Dropdown component. Input props: children is element displayed in dropdown
 export default function Dropdown({children}: {children: React.ReactNode}) {
+    // State for dropdown open/closed status. Default is closed.
     const [isOpen, setOpen] = useState(false);
 
+// Function to toggle dropdown open/closed status.
     const toggleDropdown = () => {
         setOpen(!isOpen);
     }
 
     return (
         <div className="dropdown">
+            {/* Button calls toggleDropdown to open/close the dropdown. */}
             <button onClick={toggleDropdown} className="dropdown-toggle">
+                {/* Up/Down arrow toggled based on open/closed state. */}
                 <img 
                     src={isOpen ? upArrow : downArrow} 
                     alt={isOpen ? "Close dropdown" : "Open dropdown"}
                     className="dropdown-icon"
                 />
             </button>
+            {/* If dropdown is open, it displays children content. */}
             {isOpen && <div className="dropdown-content">{children}</div>}
         </div>
     )
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,11 @@
 import Dropdown from '../Dropdown/Dropdown';
 import {Link} from 'react-router';
 import './Home.css';
-
+//  Homepage. Website starts here and displays information about the website.
 export default function Home() {    
   return (
     <>
+    {/* Homepage Title and description. */}
     <header className='home-header'>
         <h1 className='header-title'>Task Management Made Easy</h1>
         <p>
@@ -12,13 +13,16 @@ export default function Home() {
             Blanditiis assumenda, dolorum deserunt sequi
             voluptatem cupiditate quo exercitationem sunt labore.
         </p>
-
+        {/* Secondary link to draw users to task page. */}
         <button className='header-link'><Link to="/tasks">Find out more!</Link></button>
     </header>
 
+    {/* Main content area. Contains website goals and supporting statements. */}
     <main className='home-info'>
         <section className='home-container'>
             <h2 className='main-header'> Goal </h2>
+            {/* Dropdown components display their children when opened. More info
+            in Dropdown.tsx. */}
             <Dropdown>
                 <p className='home-item'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ut ipsum quos consequuntur obcaecati soluta aspernatur aliquid est earum ad. Facilis omnis maxime quaerat quis nam reprehenderit architecto unde animi!
                 A atque sapiente reiciendis nisi aperiam ducimus, ipsam veniam vitae tempora vero delectus fugiat magni quod aliquid facere quia maxime voluptatem temporibus nihil possimus sunt? Soluta deleniti quasi facilis tenetur.
@@ -27,6 +31,8 @@ export default function Home() {
         </section>
         <section className='home-container'>
             <h2 className='main-header'> Supporting Statement</h2>
+            {/* Dropdown components display their children when opened. More info
+            in Dropdown.tsx. */}
             <Dropdown>
                 <p className='home-item'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, ut ipsum quos consequuntur obcaecati soluta aspernatur aliquid est earum ad. Facilis omnis maxime quaerat quis nam reprehenderit architecto unde animi!
                 A atque sapiente reiciendis nisi aperiam ducimus, ipsam veniam vitae tempora vero delectus fugiat magni quod aliquid facere quia maxime voluptatem temporibus nihil possimus sunt? Soluta deleniti quasi facilis tenetur.
